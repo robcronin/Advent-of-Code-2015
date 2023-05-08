be0f76367f8f8f2478b4d41dde165f2c352677fa
robcronin <robc@theodo.co.uk>
2023-05-08 21:38:53 +0100

    Add day 12

---
+++ src/day12/day12.ts
@@ -0,0 +1,42 @@
+import { sumArr } from '../utils/array';
+
+const countSumNumString = (input: string) => {
+  let sum = 0;
+  for (let i = 0; i < input.length; i++) {
+    const char = input[i];
+    let multiplier = 1;
+    if (Number.isInteger(+char)) {
+      if (input[i - 1] === '-') multiplier = -1;
+      let offset = 1;
+      while (Number.isInteger(+input[i + offset])) {
+        offset++;
+      }
+      const num = +input.slice(i, i + offset);
+      sum += num * multiplier;
+      i += offset - 1;
+    }
+  }
+  return sum;
+};
+
+const recursiveCountSumJson = (
+  obj: Object | Array<any> | number | string,
+): number => {
+  if (typeof obj === 'object') {
+    if (Array.isArray(obj))
+      return sumArr(obj, (arr) => recursiveCountSumJson(arr));
+    if (Object.values(obj).includes('red')) return 0;
+    return recursiveCountSumJson(Object.values(obj));
+  } else {
+    if (typeof obj === 'number') return obj;
+  }
+  return 0;
+};
+
+export const day12 = (input: string) => {
+  return countSumNumString(input);
+};
+
+export const day12part2 = (input: string) => {
+  return recursiveCountSumJson(JSON.parse(input));
+};
